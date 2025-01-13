8d7a39a97c2b28abfe30a39b6534a0de8b6f1461
worldofhope <n.sadova98@gmail.com>
2025-01-13 14:25:40 +0500

    Update OrderDetails.tsx
    Получение и обновление номера заказа в модальном окне OrderDetails.

--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 import done from '../../images/done.svg';
 
 import s from './OrderDetails.module.scss';
 
+import { setOrderDetails } from '../../services/order/slice';
+
 export default function OrderDetails() {
+  const orderDetails = useSelector((state) => state.order.orderDetails);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!orderDetails) {
+      dispatch(setOrderDetails({ number: '034536' }));
+    }
+  }, [dispatch, orderDetails]);
+
+  if (!orderDetails) {
+    return <div>Загрузка...</div>; 
+  }
+
   return (
     <>
-      <div className={classNames(s.order__number, 'mb-8 text_type_digits-large')}>034536</div>
+      <div className={classNames(s.order__number, 'mb-8 text_type_digits-large')}>{orderDetails.number}</div>
       <span className='mb-15 text_type_main-medium'>идентификатор заказа</span>
       <div className={classNames(s.order__icon, 'mb-15')}>
         <img src={done} alt="Order Image" />
