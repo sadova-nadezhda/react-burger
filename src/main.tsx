09e699b07dfb506b21ff2807d6b275e89ddc0c7e
worldofhope <n.sadova98@gmail.com>
2025-01-13 18:02:57 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './services/store';
 import App from './components/App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = createRoot(rootElement);
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
