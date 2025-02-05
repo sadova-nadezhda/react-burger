30b0ffeb82b05334f035676cbe828cb2ebacc4da
worldofhope <n.sadova98@gmail.com>
2025-02-05 15:39:02 +0500

    Update AppHeader.tsx

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; 
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { useAppSelector } from '../../hooks/store';
@@ -19,7 +19,7 @@ interface NavItemProps {
 const NavItem = ({ id, IconComponent, label, activeId, setActiveId, to }: NavItemProps) => (
   <li
     className={classNames(s.header__item, { [s.active]: activeId === id }, 'p-5')}
-    onClick={ () => setActiveId(id) }
+    onClick={() => setActiveId(id)}
   >
     <Link to={to} className={s.header__link}>
       <IconComponent type={activeId === id ? 'primary' : 'secondary'} />
@@ -31,38 +31,64 @@ const NavItem = ({ id, IconComponent, label, activeId, setActiveId, to }: NavIte
 function AppHeader() {
   const [activeId, setActiveId] = useState(1);
   const isAuthenticated = useAppSelector((state: RootState) => !!state.auth.user);
+  const location = useLocation();
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case '/':
+        setActiveId(1);
+        break;
+      case '/orders':
+        setActiveId(2);
+        break;
+      case '/login':
+      case '/register':
+      case '/forgot-password':
+      case '/reset-password':
+        setActiveId(4);
+        break;
+      case '/profile':
+      case '/profile/orders':
+      case '/profile/orders/:number':
+        setActiveId(4);
+        break;
+      default:
+        setActiveId(1);
+        break;
+    }
+  }, [location.pathname]);
 
   return (
     <header className={classNames(s.header, 'text', 'text_type_main-default')}>
       <div className={s.container}>
         <nav className={classNames(s.header__container, 'pb-4', 'pt-4')}>
           <ul className={s.header__menu}>
-            <NavItem 
-              id={1} 
-              IconComponent={BurgerIcon} 
-              label="Конструктор" 
-              activeId={activeId} 
-              setActiveId={setActiveId} 
-              to="/" 
+            <NavItem
+              id={1}
+              IconComponent={BurgerIcon}
+              label="Конструктор"
+              activeId={activeId}
+              setActiveId={setActiveId}
+              to="/"
             />
-            <NavItem 
-              id={2} 
-              IconComponent={ListIcon} 
-              label="Лента заказов" 
-              activeId={activeId} 
-              setActiveId={setActiveId} 
-              to="/orders" 
+            <NavItem
+              id={2}
+              IconComponent={ListIcon}
+              label="Лента заказов"
+              activeId={activeId}
+              setActiveId={setActiveId}
+              to="/orders"
             />
             <li className={s.header__logo}>
               <Logo />
             </li>
-            <NavItem 
-              id={4} 
-              IconComponent={ProfileIcon} 
-              label="Личный кабинет" 
-              activeId={activeId} 
-              setActiveId={setActiveId} 
-              to={isAuthenticated ? "/profile" :"/login" }
+            <NavItem
+              id={4}
+              IconComponent={ProfileIcon}
+              label="Личный кабинет"
+              activeId={activeId}
+              setActiveId={setActiveId}
+              to={isAuthenticated ? '/profile' : '/login'}
             />
           </ul>
         </nav>
